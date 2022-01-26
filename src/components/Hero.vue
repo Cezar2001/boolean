0c00b0e1c477ff2154870eecae1f3ef1bab4b57f
Cezar <cezarlantosgmail.com>
2022-01-26 16:13:13 +0100

    Bonus

---
+++ src/components/Hero.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="jumbotron">
+        <div class="box red"></div>
+        <div class="box yellow"></div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+.jumbotron{
+    background-color: #c0c0c0;
+    height: 100vh;
+    margin-top: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.box{
+    width: 200px;
+    height: 100px;
+}
+
+.red{
+    background-color: red;
+}
+
+.yellow{
+    background-color: yellow;
+}
+
+</style>
